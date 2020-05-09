b7329cf7626c4a00bf3711d37bdba33041bb1542
Diego Anccas <diego.anccas@gmail.com>
2020-05-09 00:31:30 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/upc/gymfit/fragments/auth/AuthFragmentFactory.kt
@@ -0,0 +1,45 @@
+package pe.edu.upc.gymfit.fragments.auth
+
+import androidx.fragment.app.FragmentFactory
+import androidx.lifecycle.ViewModelProvider
+import pe.edu.upc.gymfit.di.auth.AuthScope
+import pe.edu.upc.gymfit.ui.auth.ForgotPasswordFragment
+import pe.edu.upc.gymfit.ui.auth.LauncherFragment
+import pe.edu.upc.gymfit.ui.auth.LoginFragment
+import pe.edu.upc.gymfit.ui.auth.RegisterFragment
+import javax.inject.Inject
+
+@AuthScope
+class AuthFragmentFactory
+@Inject
+constructor(
+    private val viewModelFactory: ViewModelProvider.Factory
+) : FragmentFactory() {
+
+    override fun instantiate(classLoader: ClassLoader, className: String) =
+
+        when (className) {
+
+            LauncherFragment::class.java.name -> {
+                LauncherFragment(viewModelFactory)
+            }
+
+            LoginFragment::class.java.name -> {
+                LoginFragment(viewModelFactory)
+            }
+
+            RegisterFragment::class.java.name -> {
+                RegisterFragment(viewModelFactory)
+            }
+
+            ForgotPasswordFragment::class.java.name -> {
+                ForgotPasswordFragment(viewModelFactory)
+            }
+
+            else -> {
+                LauncherFragment(viewModelFactory)
+            }
+        }
+
+
+}
