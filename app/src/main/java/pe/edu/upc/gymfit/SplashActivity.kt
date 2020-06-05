acaac79592984c3084cc1364349b03f5de94b136
Diego Anccas <diego.anccas@gmail.com>
2020-06-04 23:05:58 -0500

    Revert "Commit splashActivity"
    This reverts commit 7aa9fb24

--- app/src/main/java/pe/edu/upc/gymfit/SplashActivity.kt
+++ app/src/main/java/pe/edu/upc/gymfit/SplashActivity.kt
@@ -3,7 +3,6 @@ package pe.edu.upc.gymfit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintSet.WRAP_CONTENT
@@ -73,22 +72,7 @@ class SplashActivity : AppCompatActivity() {
             }
         }
     }
-    private fun hideSystemUI() {
-        val decorView = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                // Hide the nav bar and status bar
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
-    }
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            hideSystemUI()
-        }
-    }
+
     private fun setupIndicator(){
         val  indicators = arrayOfNulls<ImageView>(introSlideAdapter.itemCount)
         val layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
--- app/src/main/java/pe/edu/upc/gymfit/api/main/responses/BlogListSearchResponse.kt
+++ app/src/main/java/pe/edu/upc/gymfit/api/main/responses/BlogListSearchResponse.kt
@@ -4,6 +4,10 @@ import pe.edu.upc.gymfit.models.BlogPost
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Class for modeling the response when querying https://open-api.xyz/
+ * See example response here: https://gist.github.com/mitchtabian/ae03573737067c9269701ea662460205
+ */
 class BlogListSearchResponse(
 
     @SerializedName("results")
